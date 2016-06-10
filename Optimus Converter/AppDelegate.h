4b3f25c8015cec5e5a67136753333972768d065a
tejada7 <Favio@favios-imac.local>
2016-06-10 16:26:53 +0200

    Initial Commit

---
+++ Optimus Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Optimus Converter
+//
+//  Created by Favio on 6/10/16.
+//  Copyright © 2016 Favio Tejada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Optimus Converter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Optimus Converter
+//
+//  Created by Favio on 6/10/16.
+//  Copyright © 2016 Favio Tejada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
